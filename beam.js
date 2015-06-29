233b1f93a72069618ec791f62424cdfd61c19232
Tromino <idioticdinner@gmail.com>
2015-06-29 13:56:07 -0700

    Added BeamLinkDev support

--- beam.js
+++ beam.js
@@ -2,7 +2,8 @@ $("body").on("DOMNodeInserted", ".messages .nano-content", function() {
 	var i = $(".message:last-child .message-author");
 	var j = $(".message:last-child .message-body");
 	
-	if(i.clone().children().remove().end().text().toLowerCase() == "beamlink" &&
+	if((i.clone().children().remove().end().text().toLowerCase() == "beamlink" ||
+	i.clone().children().remove().end().text().toLowerCase() == "beamlinkdev") &&
 	j.html().slice(0, 1) == "[") {
 		$(".message:last-child").removeClass("message-role-BExBot");
 		i.html("<img src='http://www.twitch.tv/favicon.ico' style='margin-right: 8px;'>" + j.html().slice(1, j.html().indexOf("]")));
--- twitch.js
+++ twitch.js
@@ -2,7 +2,8 @@ $("body").on("DOMNodeInserted", ".chat-lines", function() {
 	var i = $(".chat-line:last-child .from");
 	var j = $(".chat-line:last-child .message");
 	
-	if(i.text().toLowerCase() == "beamlink" &&
+	if((i.text().toLowerCase() == "beamlink" ||
+	i.text().toLowerCase() == "beamlinkdev") &&
 	j.html().slice(0, 1) == "[") {
 		i.html("<img src='https://beam.pro/img/favicon/favicon-16x16.png' style='margin-right: 6px;'>" + j.html().slice(1, j.html().indexOf("]")));
 		i.css("color", "#37e4ee");
