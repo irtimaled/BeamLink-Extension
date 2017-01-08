b3f342c324dfb3f715ca87258909b5a32992777d
irtimaled <irtimaled@gmail.com>
2017-01-08 20:02:41 +0000

    Fix to work with changes with twitch and beam

--- beam.js
+++ beam.js
@@ -1,13 +1,16 @@
-$("body").on("DOMNodeInserted", ".messages .nano-content", function() {
-	var i = $(".message:last-child .message-author");
-	var j = $(".message:last-child .message-body");
-	
-	if((i.clone().children().remove().end().text().toLowerCase() == "beamlink" ||
-	i.clone().children().remove().end().text().toLowerCase() == "beamlinkdev") &&
-	j.html().slice(0, 1) == "[") {
-		$(".message:last-child").removeClass("message-role-BExBot");
-		i.html("<img src='http://www.twitch.tv/favicon.ico' style='margin-right: 8px;'>" + j.html().slice(1, j.html().indexOf("]")));
-		i.css("color", "#9b72ca");
-		j.html(j.html().slice(j.html().indexOf("]") + 2));
+$("body").on("DOMNodeInserted", "beam-chat-messages div", function() {
+	var i = $("beam-chat-message:last beam-chat-author");
+	var j = $("beam-chat-message:last .body span span");
+
+	if(i.text().toLowerCase() == "beamlink") {
+		setTimeout(function(){
+			var jhtml = j.html().trim();
+			if(jhtml.slice(0, 1) == "[") {
+				i.html("<img src='https://www.twitch.tv/favicon.ico' style='margin-right: 10px;'><b>" + j.html().slice(1, j.html().indexOf("]"))+"</b>");
+				i.css("color", "#9b72ca");
+				i.parent().css("padding-left", "5px");
+				j.html(jhtml.slice(jhtml.indexOf("]") + 2));
+			}
+		}, 1);
 	}
 });
--- twitch.js
+++ twitch.js
@@ -1,12 +1,13 @@
 $("body").on("DOMNodeInserted", ".chat-lines", function() {
 	var i = $(".chat-line:last-child .from");
 	var j = $(".chat-line:last-child .message");
-	
-	if((i.text().toLowerCase() == "beamlink" ||
-	i.text().toLowerCase() == "beamlinkdev") &&
-	j.html().slice(0, 1) == "[") {
-		i.html("<img src='https://beam.pro/img/favicon/favicon-16x16.png' style='margin-right: 6px;'>" + j.html().slice(1, j.html().indexOf("]")));
-		i.css("color", "#37e4ee");
-		j.html(j.html().slice(j.html().indexOf("]") + 2));
+
+	if(i.text().toLowerCase() == "beamlink") {
+		var jhtml = j.html().trim();
+		if(jhtml.slice(0, 1) == "[") {
+			i.html("<img src='https://beam.pro/_latest/img/favicon/favicon-16x16.png' style='margin-right: 6px;'>" + jhtml.slice(1, jhtml.indexOf("]")));
+			i.css("color", "#37e4ee");
+			j.html(jhtml.slice(jhtml.indexOf("]") + 2));
+		}
 	}
 });
