03d982f805d8b2a1879180a3b387ec2dfb576e8b
Irtimaled <irtimaled@gmail.com>
2019-02-20 07:09:39 -0800

    Final changes

--- beam.js
+++ beam.js
@@ -1,16 +1,26 @@
 $("body").on("DOMNodeInserted", "beam-chat-messages div", function() {
-	var i = $("beam-chat-message:last beam-chat-author");
-	var j = $("beam-chat-message:last .body span span");
-
-	if(i.text().toLowerCase() == "beamlink") {
-		setTimeout(function(){
-			var jhtml = j.html().trim();
-			if(jhtml.slice(0, 1) == "[") {
-				i.html("<img src='https://www.twitch.tv/favicon.ico' style='margin-right: 10px;'><b>" + j.html().slice(1, j.html().indexOf("]"))+"</b>");
-				i.css("color", "#9b72ca");
-				i.parent().css("padding-left", "5px");
-				j.html(jhtml.slice(jhtml.indexOf("]") + 2));
-			}
-		}, 1);
-	}
+  var beamChatMessage =  $("beam-chat-message:last");
+  var i = $(beamChatMessage).find("beam-chat-author");
+  var j = $(beamChatMessage).find(".body span span");
+  if(i.text().toLowerCase() == "chatlinker") {
+    setTimeout(function(){
+      var jhtml = j.html().trim();
+      if(jhtml.slice(0, 1) == "[") {
+        var username = jhtml.slice(1, jhtml.indexOf("]"));
+        var message = jhtml.slice(jhtml.indexOf("]") + 2);
+        if(username.slice(0, 1) == "/") {
+          username = "<i>"+username.slice(1)+"</i>";
+          message = "<i>"+message+"</i>";
+        }
+        else
+        {
+          username = "<b>"+username+"<b>";
+        }
+        i.html("<img src='https://www.twitch.tv/favicon.ico' style='margin-right: 10px;'>"+username);
+        i.css("color", "#9b72ca");
+        i.parent().css("padding-left", "5px");
+        j.html(message);
+      }
+    }, 1);
+  }
 });
--- twitch.js
+++ twitch.js
@@ -1,13 +1,20 @@
 $("body").on("DOMNodeInserted", ".chat-lines", function() {
-	var i = $(".chat-line:last-child .from");
-	var j = $(".chat-line:last-child .message");
-
-	if(i.text().toLowerCase() == "beamlink") {
-		var jhtml = j.html().trim();
-		if(jhtml.slice(0, 1) == "[") {
-			i.html("<img src='https://beam.pro/_latest/img/favicon/favicon-16x16.png' style='margin-right: 6px;'>" + jhtml.slice(1, jhtml.indexOf("]")));
-			i.css("color", "#37e4ee");
-			j.html(jhtml.slice(jhtml.indexOf("]") + 2));
-		}
-	}
+  var chatLine =  $(".chat-line:last-child");
+  var i = $(chatLine).find(".from");
+  var j = $(chatLine).find(".message");
+  if(i.text().toLowerCase() == "chatlinker") {
+    var jhtml = j.html().trim();
+    if(jhtml.slice(0, 1) == "[") {
+      var username = jhtml.slice(1, jhtml.indexOf("]"));
+      var message = jhtml.slice(jhtml.indexOf("]") + 2);
+      if(username.slice(0, 1) == "/") {
+        username = username.slice(1);
+        $(chatLine).find(".colon").remove();
+        j.css("color", "#37e4ee");
+      }
+      i.html("<img src='https://beam.pro/_latest/assets/favicons/favicon-32x32.png' style='margin-right: 6px; height: 20px;'><b>"+username+"</b>");
+      i.css("color", "#37e4ee");
+      j.html(message);
+    }
+  }
 });
